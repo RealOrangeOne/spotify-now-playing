ede8b52b3c323b40ed81366c4e0c84ad3e434775
Jake Howard <git@theorangeone.net>
2019-07-18 21:09:25 +0100

    Init web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use log::Level;
+use simple_logger;
+
+mod utils;
+use utils::get_port;
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = get_port();
+    simple_logger::init_with_level(Level::Info).unwrap();
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/", web::get().to(index))
+    })
+    .bind(format!("127.0.0.1:{}", port))
+    .unwrap()
+    .run()
+    .unwrap();
 }
